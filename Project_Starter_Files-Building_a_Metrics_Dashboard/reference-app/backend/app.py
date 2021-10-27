810b91f9ff2d844d476339bc6092dd8ff3aaf4b0
alice <bow1226@gmail.com>
2021-10-27 19:31:11 +0800

    feat: Implement endpoints and generate 40x and 50x error API Exceptions

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/backend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/backend/app.py
@@ -1,14 +1,45 @@
 from flask import Flask, render_template, request, jsonify
-
+import time
+import random
 import pymongo
 from flask_pymongo import PyMongo
-
+from prometheus_flask_exporter import PrometheusMetrics
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'example-mongodb'
 app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb'
 
 mongo = PyMongo(app)
+PrometheusMetrics(app)
+
+class InvalidHandle(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        error_message = dict(self.payload or ())
+        error_message['message'] = self.message
+        return error_message
+
+@app.route('/error')
+def oops():
+    return ':(', 500
+
+@app.errorhandler(InvalidHandle)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+@app.route('/foo')
+def get_error():
+    raise InvalidHandle('error occur', status_code=410)
 
 @app.route('/')
 def homepage():
@@ -31,4 +62,4 @@ def add_star():
   return jsonify({'result' : output})
 
 if __name__ == "__main__":
-    app.run()
+    app.run(threaded=True)
--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+import threading
+import requests
+import random
+import time
 
 app = Flask(__name__)
+endpoints = ('error', 'foo')
+
+def random_endpoint():
+        try:
+            target = random.choice(endpoints)
+            requests.get("http://app:8080/%s" % target, timeout=1)
+
+        except:
+            pass
 
 @app.route('/')
 def homepage():
     return render_template("main.html")
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  
+    
     app.run()
+    threading.Thread(target=random_endpoint).start()
+    
