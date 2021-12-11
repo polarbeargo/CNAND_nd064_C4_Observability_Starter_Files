c40d5a267741bd023ffd25e9631ec56511cab393
alice <bow1226@gmail.com>
2021-12-11 10:29:44 +0800

    test: Simple request generator.sh

---
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/generator.sh
@@ -0,0 +1,4 @@
+for i in 0 1 2 3 4 5 6 7 8 9; do curl localhost:8081/error; done
+for i in 0 1 2 3 4 5 6 7 8 9; do curl localhost:8081/star; done
+for i in 0 1 2 3 4 5 6 7 8 9; do curl localhost:8081/api; done
+
